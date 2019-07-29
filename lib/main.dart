8252b760249105bb80db03305e05ff0cb7e0a922
krupikivan <krupikivan@gmail.com>
2019-07-28 22:03:07 -0300

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:fastshop/application.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-void main() {
-  debugPaintSizeEnabled = false;
-  runApp(Application());
+void main() => runApp(Application());
+
+void _get(){
+
 }
